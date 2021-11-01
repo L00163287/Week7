c8d39b877218e6c933fbe133010cf18ae456e4c1
L00163287 <L00163287@student.lyit.ie>
2021-11-01 11:04:19 +0000

    "Initial commit for Decorators and Annotations"

---
+++ Decorators/Example.py
@@ -0,0 +1,11 @@
+"""
+#
+# @File         : Example.py
+# @Created      : 2021-11-01 11:03
+# @Author       : Bubashankushan B
+# @Version      : v1.0.0
+# @Licensing    : 
+#
+# @Description  :
+#
+"""
